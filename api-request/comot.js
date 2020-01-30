52918e1bfd41df5290524b5b8ce57b98a29757e5
Withered Flowers <withered-flowers@github.com>
2020-01-30 08:11:25 +0700

    adding content

--- api-request/comot.js
+++ api-request/comot.js
@@ -1,26 +1,26 @@
 const requestDataViaAjax = (cb) => {
-    let xhr = new XMLHttpRequest();
-    let url = "https://jsonplaceholder.typicode.com/users/1";
+  let xhr = new XMLHttpRequest();
+  let url = "https://jsonplaceholder.typicode.com/users/1";
 
-    xhr.open('GET', url);
-  
-    xhr.onload = () => {
-      if(xhr.status === 200) {
-        cb(xhr.responseText);
-      }
-      else {
-        cb(null);
-      }
+  xhr.open('GET', url);
+
+  xhr.onload = () => {
+    if(xhr.status === 200) {
+      cb(xhr.responseText);
     }
-  
-    xhr.send();
-  }
-  
-  const showResult = (data) => {
-    if (data != null) {
-      data = JSON.parse(data);
-      console.log(data);
+    else {
+      cb(null);
     }
   }
+
+  xhr.send();
+}
   
-  requestDataViaAjax(showResult);
+const showResult = (data) => {
+  if (data != null) {
+    data = JSON.parse(data);
+    console.log(data);
+  }
+}
+  
+requestDataViaAjax(showResult);
